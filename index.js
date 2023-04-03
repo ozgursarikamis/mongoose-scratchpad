6cc7e3e0769a6bf0ba93dbb597432cf87e9981ee
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2023-04-03 21:48:29 +0100

    Different data types in Schema

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ run();
 
 async function run() {
     const user = new User({ name: 'John', age: 20 });
+    user.name = 'Ozgur'
+    user.hobbies = ['Reading', 'Writing', 'Coding'];
+    user.address = {
+        city: 'London',
+        country: 'United Kingdom',
+        postCode: 'W1 1AA'
+    };
+    user.bestFriend = '5e9b1b5b1c9d440000a1e1b1';
     await user.save();
     // or:
     // await User.create({ name: 'John', age: 20 });
--- schemas/User.js
+++ schemas/User.js
@@ -2,7 +2,17 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     name: String,
-    age: Number
+    age: Number,
+    email: String,
+    createdAt: Date,
+    updatedAt: Date,
+    bestFriend: mongoose.SchemaTypes.ObjectId,
+    hobbies: [String],
+    address: {
+        city: String,
+        country: String,
+        postCode: String
+    }
 }); 
 
 module.exports = mongoose.model('User', userSchema); // User collection with userSchema.
