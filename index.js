68d0e9ac131c96814e129c512959bfbf6f6263ae
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2023-04-03 21:39:34 +0100

    Saving data to database

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const mongoose = require('mongoose');
+const User = require('./schemas/User');
 
 mongoose.connect('mongodb://localhost:27017/test').then(() => {
     console.log('Connected to MongoDB');
-    }).catch((err) => {
+}).catch((err) => {
     console.log('Error connecting to MongoDB', err);
 });
+
+run();
+
+async function run() {
+    const user = new User({ name: 'John', age: 20 });
+    await user.save();
+    // or:
+    // await User.create({ name: 'John', age: 20 });
+    console.log('User saved', user);
+}
