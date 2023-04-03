ff0ce838772b0fdb3b83c4c3c8761deb4a9ef4c9
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2023-04-03 22:23:55 +0100

    find and findById method

--- index.js
+++ index.js
@@ -11,21 +11,11 @@ run();
 
 async function run() {
     try {
-        const user = new User({ name: 'John', age: 66 });
-        user.name = 'Ozgur'
-        user.email = 'OZGUR@'
-        user.hobbies = ['Reading', 'Writing', 'Coding'];
-        user.address = {
-            city: 'London',
-            country: 'United Kingdom',
-            postCode: 'W1 1AA'
-        };
-        user.bestFriend = '5e9b1b5b1c9d440000a1e1b1';
-        user.createdAt = new Date();
-        await user.save();
-        // or:
-        // await User.create({ name: 'John', age: 20 });
-        console.log('User saved', user);
+        const user = await User.findById('642b3e6307b74c0fe2004f3a');
+        console.log(user);
+        console.log("====================================");
+        const user2 = await User.find({ name: 'Halime', age: 25 });
+        console.log(user2);
     } catch (error) {
         console.log();
         console.log('Error saving user', error.message);
